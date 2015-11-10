5097208f881a378488eac76f7c8bd2e658fa7932
Alex <whitedawn1@hotmail.com>
2015-11-09 19:49:32 -0600

    Initial commit

---
+++ app/src/main/java/com/example/angel/noteboard/Note.java
@@ -0,0 +1,8 @@
+package com.example.angel.noteboard;
+
+/**
+ * Created by Angel on 11/9/2015.
+ */
+public class Note {
+
+}
---
+++ app/src/main/java/com/example/angel/noteboard/NoteListFragment.java
@@ -0,0 +1,10 @@
+package com.example.angel.noteboard;
+
+import android.app.ListFragment;
+
+/**
+ * Created by Angel on 11/9/2015.
+ */
+public class NoteListFragment extends ListFragment {
+
+}
